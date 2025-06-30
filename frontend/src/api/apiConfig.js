5f516c90d973d4e9a2924c7f4e889838d7fa1527
Ana <ana.ilievska124@gmail.com>
2025-06-30 02:49:11 +0200

    Finished

--- frontend/src/api/apiConfig.js
+++ frontend/src/api/apiConfig.js
@@ -1,12 +1,15 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8181/api';
+const API_BASE_URL = 'http://68.221.217.29/api';
+
+
 
 const apiClient = axios.create({
-    baseURL: API_BASE_URL,
+    baseURL: '/api',
     headers: {
         'Content-Type': 'application/json',
     },
 });
+
 
 export default apiClient;
